45ddf5f09156fac5b15d5cc930ca7996c3524835
miles <slion0725@gmail.com>
2016-12-26 13:11:28 +0800

    Crud

--- routes/mysqlcrud.js
+++ routes/mysqlcrud.js
@@ -20,7 +20,7 @@ var connection = mysql.createConnection({
 //   console.log('connected as id ' + connection.threadId);
 // });
 
-router.get('/:num?', function (req, res, next) {
+router.get('/list/:num?', function (req, res) {
   num = req.params.num;
 
   if (!num) {
@@ -30,10 +30,50 @@ router.get('/:num?', function (req, res, next) {
   }
 
   connection.query('SELECT * FROM mysqlcrud limit 0,?', [num], function (err, rows, fields) {
-    if (err) {
-      throw err;
-    }
+    if (err) throw err;
     res.json(rows);
+  });
+});
+
+router.get('/:id', function (req, res) {
+  id = parseInt(req.params.id);
+  connection.query('SELECT * FROM mysqlcrud WHERE id = ?', [id], function (err, rows, fields) {
+    if (err) throw err;
+    res.json(rows);
+  });
+});
+
+router.post('/', function (req, res) {
+  console.log(req.body);
+  data = {
+    name: req.body.name,
+    content: req.body.content
+  };
+  connection.query('INSERT INTO mysqlcrud SET ?', data, function (err, result) {
+    if (err) throw err;
+    connection.query('SELECT * FROM mysqlcrud WHERE id = ?', [result.insertId], function (err, rows, fields) {
+      if (err) throw err;
+      res.json(rows[0]);
+    });
+  });
+});
+
+router.put('/:id', function (req, res) {
+  id = parseInt(req.params.id);
+  data = req.body;
+  connection.query('UPDATE mysqlcrud SET ? WHERE id = ?', [
+    data, id
+  ], function (err, result) {
+    if (err) throw err;
+    res.json(req.params);
+  });
+});
+
+router.delete('/:id', function (req, res) {
+  id = parseInt(req.params.id);
+  connection.query('DELETE FROM mysqlcrud WHERE id = ?', [id], function (err, result) {
+    if (err) throw err;
+    res.json(req.params);
   });
 });
 
